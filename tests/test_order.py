760210b85504e799b689a1e9c0197c2ca24dcbd8
Reza Jugon <rezajugon@icloud.com>
2024-06-04 15:17:39 +0100

    Added working tests 2

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,6 +1,11 @@
+import sys
+sys.path.append('/Users/reza/Code/takeaway')
+
 from lib.Order import Order
 
 def test_order_total():
     myOrder = Order()
-    assert myOrder.grand_total() == 99
+    result = myOrder.grand_total()
+    
+    assert result == 99
     
